d0953167a239b6d692523cfb285cc06f18331b75
ashlym1 <ashlym3@usf.edu>
2025-04-20 17:00:40 -0400

    app.jsx: stored the data in a tours state variable

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,16 @@ useEffect(() => {
   //  Fetching  the API; Applying a proxy to avoid CORS issues 
 const url = 'https://api.allorigins.win/raw?url=https://course-api.com/react-tours-project';
 }, []);
+ // Creating a state variable to store the tours 
+const [tours, setTours] = useState([]);
+const fetchTours = async () => {
+  try { 
+    const response = await fetch(url);   // requesting the data from the API
+    const data = await response.json(); // convert to json 
+     // save the data that was fetched into the state variable
+    setTours(data);
+  } catch (err) {
+    // If there is an error it will display a error message.
+    console.log("Fetch error:", err);
+  }
+};
