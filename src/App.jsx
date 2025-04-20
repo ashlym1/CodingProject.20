912d77e7af1a4e99da79ae9226eb863b9ccf71a2
ashlym1 <ashlym3@usf.edu>
2025-04-20 16:53:07 -0400

    fetched tours using useEffects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//  Importing 
+import React, { useState,useEffect } from "react";
+import Gallery from "./Components/Gallery";
+import DestinationSelector from "./Components/DestinationSelector";
+import './Styles/style.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+ 
+useEffect(() => {
+  fetchTours(); // using affects to fetch the tours
+  //  Fetching  the API; Applying a proxy to avoid CORS issues 
+const url = 'https://api.allorigins.win/raw?url=https://course-api.com/react-tours-project';
+}, []);
